7d1c04230c8620c0fea10b8bc3e7d23e35cce278
himreaowi <himariang@gmail.com>
2023-08-22 21:30:24 +0800

    Add user auth

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -5,11 +5,13 @@ import { Button } from "./ui/Button";
 import { cn } from "@/lib/utils";
 import { signIn } from "next-auth/react";
 import { Icons } from "./Icons";
+import { useToast } from "@/hooks/use-toast";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { toast } = useToast();
 
   const loginWithGoogle = async () => {
     setIsLoading(true);
@@ -17,6 +19,11 @@ const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
     try {
       await signIn("google");
     } catch (error) {
+      toast({
+        title: "There was a problem.",
+        description: "There was an error logging in with Google",
+        variant: "destructive",
+      });
     } finally {
       setIsLoading(false);
     }
