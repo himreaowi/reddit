29f8e241cf684cfc17e2ae1e027d2457ce53a08b
himreaowi <himariang@gmail.com>
2023-09-12 12:49:09 +0800

    Create new subreddit page

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -45,6 +45,16 @@ const Page = () => {
           return loginToast();
         }
       }
+
+      toast({
+        title: "There was an error",
+        description: "Could not create subreddit.",
+        variant: "destructive",
+      });
+    },
+    onSuccess: (data) => {
+      console.log("Enter onsuccess", data);
+      router.push(`/r/${data}`);
     },
   });
 
