d18c5041a0709bff948d3d433b5e765b4f7e8e98
himreaowi <himariang@gmail.com>
2023-09-12 22:24:10 +0800

    Add Join and Leave subreddit component

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,3 +1,4 @@
+import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { format } from "date-fns";
@@ -65,12 +66,32 @@ const Layout = async ({
             <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-6 bg-white">
               <div className="flex justify-between gap-x-4 py-3">
                 <dt className="text-gray-500">Created</dt>
-                <dt className="text-gray-700">
+                <dd className="text-gray-700">
                   <time dateTime={subreddit.createdAt.toDateString()}>
                     {format(subreddit.createdAt, "MMMM d, yyyy")}
                   </time>
-                </dt>
+                </dd>
               </div>
+              <div className="flex justify-between gap-x-4 py-3">
+                <dt className="text-gray-500">Members</dt>
+                <dd className="text-gray-700">
+                  <div className="text-gray-900">{memberCount}</div>
+                </dd>
+              </div>
+
+              {subreddit.creatorId === session?.user.id ? (
+                <div className="flex justify-between gap-x-4 py-3">
+                  <p className="text-gray500">You created this community</p>
+                </div>
+              ) : null}
+
+              {subreddit.creatorId !== session?.user.id ? (
+                <SubscribeLeaveToggle
+                  isSubscribed={isSubscribed}
+                  subredditId={subreddit.id}
+                  subredditName={subreddit.name}
+                />
+              ) : null}
             </dl>
           </div>
         </div>
